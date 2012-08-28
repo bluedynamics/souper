b991f35cdb8271c46a6826f3ee7ea59c277a6845
Jens W. Klein <jens@bluedynamics.com>
2012-08-28 19:06:12 +0200

    version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0pre1'
+version = '1.0-beta1'
 shortdesc = \
 'Souper - Generic Indexed Storage based on ZODB'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
