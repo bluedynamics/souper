fbbed3f6d2322a8afdc26cbe9be4a78125b44237
Jens W. Klein <jk@kleinundpartner.at>
2015-02-25 12:56:00 +0100

    Back to development: 1.0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0.2'
+version = '1.0.3.dev0'
 shortdesc = 'Souper - Generic Indexed Storage based on ZODB'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')).read()
