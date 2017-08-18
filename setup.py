60a67ff8e21ca0e41ae6cc6afe0a410900d75a3d
Robert Niederreiter <office@squarewave.at>
2017-08-18 09:19:58 +0200

    no more test extra requires in odict

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
 tests_require = [
-    'interlude', 'node.ext.zodb[test]', 'odict[test]', 'node[test]'
+    'interlude', 'node.ext.zodb[test]', 'odict', 'node[test]'
 ]
 
 setup(name='souper',
