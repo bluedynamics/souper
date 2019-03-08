cbda4d42c10c657ff2192b471e25058a20b7f2f0
Jens W. Klein <jk@kleinundpartner.at>
2019-03-08 10:09:45 +0100

    make python2.7 setuptools and zest.releaser happy

--- setup.py
+++ setup.py
@@ -2,4 +2,11 @@
 from setuptools import setup
 
 
-setup()
+setup(
+    # zest releaser does not change cfg file.
+    version='1.1.0.dev0',
+    # thanks to this bug
+    # https://github.com/pypa/setuptools/issues/1136
+    # we need one line in here:
+    package_dir={"": "src"},
+)
