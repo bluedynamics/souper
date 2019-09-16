d6df7ad872f328b31563895612dbdcd8b1163758
Jens W. Klein <jk@kleinundpartner.at>
2019-09-16 16:56:26 +0200

    Back to development: 1.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     # zest releaser does not change cfg file.
-    version='1.1.1',
+    version='1.1.2.dev0',
     # thanks to this bug
     # https://github.com/pypa/setuptools/issues/1136
     # we need one line in here:
